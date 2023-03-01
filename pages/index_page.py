e4b92d99e383339f749c31db845bb9b4ad025c96
Alexandr <Chur10@mail.ru>
2023-03-02 02:05:07 +0300

    Add assertion for first step for Join now button

--- pages/index_page.py
+++ pages/index_page.py
@@ -3,6 +3,16 @@ import config
 
 
 class IndexPage:
+    _BUTTON_JOIN_NOW = "//html/body/nav/div/a[1]"
+    _LINK_LANGUAGES = "//html/body/footer/ul/li[11]/div/button"
+    _LINK_LANGUAGE_ENG = "//html/body/footer/ul/li[11]/div/ul/li[5]/button"
 
     def open_index_page(self, page: Page) -> None:
         page.goto(config.url.DOMAIN)
+
+    def press_link_english_lang(self, page: Page):
+        page.locator(self._LINK_LANGUAGES).click()
+        page.locator(self._LINK_LANGUAGE_ENG).click()
+
+    def get_text_from_join_now_button(self, page: Page) -> str:
+        return page.locator(self._BUTTON_JOIN_NOW).inner_text()
--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -1,10 +1,11 @@
 import pytest
 import pages
-import time
 
 
 class TestFooter:
 
     def test_main_buttons_on_main_page(self, page):
         pages.index_page.open_index_page(page)
-        time.sleep(10)
+        pages.index_page.press_link_english_lang(page)
+        actual_result = pages.index_page.get_text_from_join_now_button(page)
+        assert actual_result.strip() == 'Join now', 'Join now button text is incorrect'

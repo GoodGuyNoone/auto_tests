c0583d62e6540b24321021f5b401c59ee9cf2ff5
Alexandr <Chur10@mail.ru>
2023-03-04 00:22:24 +0300

    Added check header button on main page

--- pages/index_page.py
+++ pages/index_page.py
@@ -4,8 +4,14 @@ import config
 
 class IndexPage:
     _BUTTON_JOIN_NOW = "//html/body/nav/div/a[1]"
+    _BUTTON_SIGN_IN = "//html/body/nav/div/a[2]"
+    _BUTTON_DISCOVER = "//html/body/nav/ul/li[1]/a/span"
+    _BUTTON_PEOPLE = '//html/body/nav/ul/li[2]/a/span'
+    _BUTTON_LEARNING = '//html/body/nav/ul/li[3]/a/span'
+    _BUTTON_JOBS = '//html/body/nav/ul/li[4]/a/span'
     _LINK_LANGUAGES = "//html/body/footer/ul/li[11]/div/button"
     _LINK_LANGUAGE_ENG = "//html/body/footer/ul/li[11]/div/ul/li[5]/button"
+
 
     def open_index_page(self, page: Page) -> None:
         page.goto(config.url.MAIN_PAGE)
@@ -16,3 +22,16 @@ class IndexPage:
 
     def get_text_from_join_now_button(self, page: Page) -> str:
         return page.locator(self._BUTTON_JOIN_NOW).inner_text()
+
+    def get_text_from_sign_in_button(self, page: Page) -> str:
+        return page.locator(self._BUTTON_SIGN_IN).inner_text()
+
+    def get_text_from_header_buttons(self, page: Page) -> list:
+        header_buttons = [
+                          page.locator(self._BUTTON_DISCOVER).inner_text(),
+                          page.locator(self._BUTTON_PEOPLE).inner_text(),
+                          page.locator(self._BUTTON_LEARNING).inner_text(),
+                          page.locator(self._BUTTON_JOBS).inner_text()
+                          ]
+        return header_buttons
+
--- tests/test_index_page.py
+++ tests/test_index_page.py
@@ -8,5 +8,11 @@ class TestButtons:
         pages.index_page.open_index_page(page)
         pages.index_page.press_link_english_lang(page)
         join_now_button = pages.index_page.get_text_from_join_now_button(page)
+        sign_in_button = pages.index_page.get_text_from_sign_in_button(page)
+        header_buttons = pages.index_page.get_text_from_header_buttons(page)
         assert join_now_button.strip() == 'Join now', 'Join now button text is incorrect'
-
+        assert sign_in_button.strip() == 'Sign in', 'Sign in button text is incorrect'
+        assert 'Discover' in header_buttons, 'Discover button is incorrect'
+        assert 'People' in header_buttons, 'Discover button is incorrect'
+        assert 'Learning' in header_buttons, 'Discover button is incorrect'
+        assert 'Jobs' in header_buttons, 'Discover button is incorrect'

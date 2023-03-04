2bcc8022874f74b7aae66e87731fddd2a73ae264
Alexandr <Chur10@mail.ru>
2023-03-04 19:45:42 +0300

    minor сщккуÑcorrections

--- api/qase_api.py
+++ api/qase_api.py
@@ -11,16 +11,15 @@ class QaseApi:
     DOMAIN = f'{config.qase.API_DOMAIN}/{config.qase.API_VERSION}'
     PROJECT = config.qase.PROJECT
 
-@staticmethod
-def check_response(response) -> bool:
-    json_response = response.json()
-    if response.status != 200 or not json_response['status']:
-        return False
-    else:
-        return True
+    @staticmethod
+    def check_response(response) -> bool:
+        json_response = response.json()
+        if response.status_code != 200 or not json_response['status']:
+            return False
+        else:
+            return True
 
-
-def update_case(self, case_id: int, payload: dict) -> bool:
-    url = f'{self.DOMAIN}/case/{self.PROJECT}/{case_id}'
-    response = requests.patch(url, json=payload, headers=self.HEADERS)
-    return self.check_response(response)
+    def update_case(self, case_id: int, payload: dict) -> bool:
+        url = f'{self.DOMAIN}/case/{self.PROJECT}/{case_id}'
+        response = requests.patch(url, json=payload, headers=self.HEADERS)
+        return self.check_response(response)
--- fixtures/qase_fixtures.py
+++ fixtures/qase_fixtures.py
@@ -5,10 +5,10 @@ import qase
 import api
 
 
-@pytest.fixtures(autouse=config.qase.IS_DOC_UPDATE_NEEDED)
+@pytest.fixture(autouse=config.qase.IS_DOC_UPDATE_NEEDED)
 def update_docs(request) -> None:
     case_id_marker = request.node.get_closest_marker('case_id')
-    if config.qase.TOKEN and len(case_id_marker) > 0:
+    if config.qase.TOKEN and len(case_id_marker.args) > 0:
         qase.docs.create_database(request)
     yield
     if config.qase.TOKEN and len(case_id_marker.args) > 0:

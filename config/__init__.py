78cb48e1dfb55789670e3832ca9024534ffeb4c6
Alexandr <Chur10@mail.ru>
2023-03-04 19:16:12 +0300

    Added statement for Token and update_needed

--- config/__init__.py
+++ config/__init__.py
@@ -1,7 +1,10 @@
 from config.expectations import Expectations
 from config.playwright import Playwright
 from config.url import Url
+from config.qase import Qase
+
 
 url = Url()
 playwright = Playwright()
 expectations = Expectations()
+qase = Qase()

55e1ef9e5717ca6226440fe432c4c7aec62579ce
Alexandr <Chur10@mail.ru>
2023-03-06 18:09:57 +0300

    Adde try-finally

--- selenium_stepik/1_6_3_Working_with_browser.py
+++ selenium_stepik/1_6_3_Working_with_browser.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-
 link = "http://suninjuly.github.io/simple_form_find_task.html"
 browser = webdriver.Chrome()
-browser.get(link)
-button = browser.find_element(By.ID, "submit_button")
-button.click()
 
-browser.quit()
+try:
+    browser.get(link)
+    button = browser.find_element(By.ID, "submit_button")
+    button.click()
+
+finally:
+    browser.quit()

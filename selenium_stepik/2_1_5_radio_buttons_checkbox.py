60487624a3c9d52daf7f2081ad9784db484ddc1c
Alexandr <Chur10@mail.ru>
2023-03-07 20:06:01 +0300

    completed 2_1_5

---
+++ selenium_stepik/2_1_5_radio_buttons_checkbox.py
@@ -0,0 +1,31 @@
+import math
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+
+link = 'https://suninjuly.github.io/math.html'
+browser = webdriver.Chrome()
+
+
+try:
+    browser.get(link)
+    x = browser.find_element(By.CSS_SELECTOR, 'span#input_value')
+    x = x.text
+    y = calc(x)
+    input_field = browser.find_element(By.CSS_SELECTOR, 'input#answer')
+    input_field.send_keys(y)
+    robot_checkbox = browser.find_element(By.CSS_SELECTOR, '#robotCheckbox')
+    robot_checkbox.click()
+    robot_radio = browser.find_element(By.CSS_SELECTOR, '#robotsRule')
+    robot_radio.click()
+    submit_button = browser.find_element(By.CSS_SELECTOR, 'button.btn.btn-default')
+    submit_button.click()
+finally:
+    time.sleep(10)
+    browser.quit()
